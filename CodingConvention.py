a41969230191de15c6c067c2f206bedc71afeacc
VinhBao <levinhbao410@gmail.com>
2024-10-01 17:12:46 +0700

    Sua loi thut le

--- CodingConvention.py
+++ CodingConvention.py
@@ -8,5 +8,5 @@ def    calc(a, b): # Tên hàm không mô tả rõ chức năng và thụt lề 
 def CALCULATEtotalprice(itemlist): # Tên hàm không tuân theo quy tắc đặt tên thống nhất
     total = 0
     for i in itemlist:
-                total+=i.price # thụt lề quá sâu
+        total+=i.price 
     return total
